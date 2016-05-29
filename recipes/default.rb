829b210e276c622efe96cb7a15ed569bccc5d035
Pedro Tanaka <pedro.stanaka@gmail.com>
2016-05-29 17:43:02 -0300

    Adding ZSH, vim and OMZSH

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,17 +9,35 @@
 
 
 
-group node['php_chef']['group']
 
-user node['php_chef']['user'] do
-  system true
-  group node['php_chef']['group']
-  shell '/bin/bash'
-end
 
 
 include_recipe 'nginx'
 include_recipe 'vim'
 include_recipe 'zsh'
+include_recipe 'lxmx_oh_my_zsh'
 
+
+# Users and Groups Stuff...
+group node['php_chef']['group']
+
+user node['php_chef']['user'] do
+  system true
+  group node['php_chef']['group']
+  shell '/usr/bin/zsh'
+end
+
+lxmx_oh_my_zsh_user node['php_chef']['user'] do
+  plugins        %w{git ruby rvm}
+  home 			 "/home/#{node['php_chef']['user']}"
+  autocorrect    false
+  case_sensitive true
+end
+
+lxmx_oh_my_zsh_user vagrant do
+  plugins        %w{git ruby rvm}
+  home 			 "/home/#{node['php_chef']['user']}"
+  autocorrect    false
+  case_sensitive true
+end
 
