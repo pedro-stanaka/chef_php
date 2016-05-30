fa9fd4dc5f4045be03899de9e7167c542a149a2a
Pedro Tanaka <pedro.stanaka@gmail.com>
2016-05-29 23:31:56 -0300

    Adding simple html page to nginx and adding VirtualHosts.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
 
 default['php_chef']['user'] = 'deploy'
 default['php_chef']['group'] = 'deploy'
+
+default['php_chef']['appname'] = 'myapp'
+default['php_chef']['servername'] = 'myapp.local'
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,14 +8,14 @@
 #
 
 
-
-
-
-
+# Including recipes
 include_recipe 'nginx'
 include_recipe 'vim'
 include_recipe 'zsh'
 include_recipe 'lxmx_oh_my_zsh'
+include_recipe 'tmux'
+
+
 
 
 # Users and Groups Stuff...
@@ -24,20 +24,81 @@ group node['php_chef']['group']
 user node['php_chef']['user'] do
   system true
   group node['php_chef']['group']
+  group 'www-data'
   shell '/usr/bin/zsh'
 end
 
+group 'www-data' do
+  action :modify
+  members ['vagrant', node['php_chef']['user']]
+  append true
+end
+
+
 lxmx_oh_my_zsh_user node['php_chef']['user'] do
-  plugins        %w{git ruby rvm}
+  plugins        %w{git ruby rvm bower debian node npm tmux}
   home 			 "/home/#{node['php_chef']['user']}"
+  theme          "steeef"
   autocorrect    false
   case_sensitive true
 end
 
 lxmx_oh_my_zsh_user 'vagrant' do
-  plugins        %w{git ruby rvm}
-  home 			 "/home/#{node['php_chef']['user']}"
+  plugins        %w{git ruby rvm bower debian node npm tmux}
+  home 			     "/home/vagrant"
+  theme          "steeef"
   autocorrect    false
   case_sensitive true
 end
 
+# Networking
+
+hostsfile_entry '127.0.0.1' do 
+  hostname "#{node['php_chef']['servername']}"
+  action :append
+end
+
+# Nginx Configuration
+
+template "#{node['nginx']['dir']}/sites-available/#{node['php_chef']['appname']}" do
+  source 'nginx-basic.conf.erb'
+  notifies :restart, 'service[nginx]'
+end
+
+
+directory "/var/www/#{node['php_chef']['appname']}" do
+  mode '0755'
+  action :create
+  recursive true
+end
+
+directory "/var/www/logs/" do
+  user 'www-data'
+  group 'www-data'
+  mode '0755'
+  action :create
+  recursive true
+end
+
+
+directory "/var/www/logs/#{node['php_chef']['appname']}" do
+  user 'www-data'
+  group 'www-data'
+  mode '0755'
+  action :create
+  recursive true
+end
+
+
+cookbook_file "/var/www/#{node['php_chef']['appname']}/index.html" do
+  mode '0644'
+end
+
+nginx_site "default" do 
+  enable false
+end
+
+nginx_site "#{node['php_chef']['appname']}" do 
+  enable true
+  notifies :restart, 'service[nginx]'
+end
