b8e367a06aa356b23a871735589f2df1237fc399
Pedro Tanaka <pedro.stanaka@gmail.com>
2016-05-30 00:04:57 -0300

    Version bump to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
 name             'php_chef'
 maintainer       'Pedro Tanaka'
-maintainer_email 'pedro.stanaka@gmail.com'
-license          'All rights reserved'
+maintainer_email 'me@pedrotanaka.com.br'
+license          'Apache 2.0'
 description      'Installs/Configures LEMP Stack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
 
 
 depends 'nginx', '~> 2.7.6'
