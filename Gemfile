31425b3f1ed07a9d8c0d9b66d9b6559d436cc21c
Pedro Tanaka <pedro.stanaka@gmail.com>
2016-06-01 14:33:27 -0300

    Fixing issues detected by rubocop

--- Gemfile
+++ Gemfile
@@ -3,8 +3,6 @@ source 'https://rubygems.org'
 gem 'test-kitchen'
 gem 'kitchen-vagrant'
 
-
-
 group :lint do
   gem 'foodcritic', '~> 6.3'
   gem 'rubocop', '~> 0.40.0'
--- recipes/database.rb
+++ recipes/database.rb
@@ -3,20 +3,19 @@ include_recipe 'mariadb::server'
 include_recipe 'mariadb::client'
 include_recipe 'mysql2_chef_gem::mariadb'
 
-
 mysql_database node['php_chef']['database']['dbname'] do
   connection(
-    :host => node['php_chef']['database']['host'],
-    :username => node['php_chef']['database']['username'],
-    :password => node['php_chef']['database']['password']
+    host: node['php_chef']['database']['host'],
+    username: node['php_chef']['database']['username'],
+    password: node['php_chef']['database']['password']
   )
 end
 
 mysql_database_user node['php_chef']['database']['app']['username'] do
   connection(
-    :host => node['php_chef']['database']['host'],
-    :username => node['php_chef']['database']['username'],
-    :password => node['php_chef']['database']['password']
+    host: node['php_chef']['database']['host'],
+    username: node['php_chef']['database']['username'],
+    password: node['php_chef']['database']['password']
   )
   password node['php_chef']['database']['app']['password']
   database_name node['php_chef']['database']['dbname']
