b6baf85150527d5089e89ab9ba973d625fe1a138
Pedro Tanaka <pedro.stanaka@gmail.com>
2016-06-04 15:26:10 -0300

    Reorganizing metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -8,21 +8,27 @@ source_url       'https://github.com/pedro-stanaka/php_chef'
 issues_url       'https://github.com/pedro-stanaka/php_chef/issues'
 version          '0.1.0'
 
+
+# WebServer
 depends 'user', '~> 0.4.2'
 depends 'php', '~> 1.9.0'
 depends 'nginx', '~> 2.7.6'
+
+# Database
+depends 'database', '~> 5.1.2'
+depends 'mysql2_chef_gem', '= 0.1.0'
+depends 'postgresql', '~> 4.0.6'
+depends 'mariadb', '~> 0.3.1'
+
+# Utilities
 depends 'vim', '~> 2.0.1'
 depends 'htop', '~> 2.0.0'
 depends 'git', '~> 4.5.0'
 depends 'curl'
 depends 'zlib'
 depends 'zsh', '~> 2.0.0'
+depends 'lxmx_oh_my_zsh', '~> 0.5.0'
 depends 'tmux', '~> 1.5.0'
 depends 'rvm', '~> 0.9.4'
-depends 'lxmx_oh_my_zsh', '~> 0.5.0'
 depends 'hostsfile', '~> 2.4.5'
-depends 'mariadb', '~> 0.3.1'
-depends 'database', '~> 5.1.2'
-depends 'mysql2_chef_gem', '= 0.1.0'
 depends 'nodejs', '~> 2.4.4'
-depends 'postgresql', '~> 4.0.6'
