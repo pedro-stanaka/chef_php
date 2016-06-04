ee2fda66024537cea417ad469e6df34a335d0582
Pedro Tanaka <pedro.stanaka@gmail.com>
2016-06-04 10:52:30 -0300

    Adding NodeJS, NPM and default NPM packages.

--- recipes/utilities.rb
+++ recipes/utilities.rb
@@ -1,3 +1,11 @@
 # Installs and configures rvm
 
 include_recipe 'rvm::user'
+include_recipe 'nodejs'
+
+
+nodejs_npm 'grunt'
+nodejs_npm 'gulp'
+nodejs_npm 'coffee-script'
+nodejs_npm 'bower'
+nodejs_npm 'yo'
