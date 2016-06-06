c56601b2153047ed708722457cf471d40286c5d9
Pedro Tanaka <pedro.stanaka@gmail.com>
2016-06-05 23:56:27 -0300

    Adding locale recipe only if platform is ubuntu.

--- recipes/bootstraping.rb
+++ recipes/bootstraping.rb
@@ -8,7 +8,7 @@ include_recipe 'htop'
 include_recipe 'git'
 include_recipe 'curl'
 include_recipe 'zlib'
-include_recipe 'locale'
+include_recipe 'locale' if platform?('ubuntu')
 include_recipe 'user::data_bag'
 include_recipe 'lxmx_oh_my_zsh'
 
